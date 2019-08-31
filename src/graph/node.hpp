8b1ebdd15880686b1c7c7b3fd1b7f51b4fff1940
Sean Collins <sgc29@zips.uakron.edu>
2019-08-30 21:27:17 -0400

    Clean up node code

--- src/graph/node.hpp
+++ src/graph/node.hpp
@@ -3,40 +3,30 @@
 namespace graph
 {
 
-	template <typename Key>
+	template <typename Key_t>
 	class Node
 	{
-
 	private:
-		Key key;
-
+		Key_t key;
 	public:
-
-		Node (Key key)
-		: key(key)
-		{}
-
-		Key get_key () const
-		{
-			return key;
-		}
-
+		Node (Key_t key) : key(key) {}
+		Key_t get_key () const { return key; }
 	};
 
-  template <typename Key>
-  bool operator == (Node<Key> const & lhs, Node<Key> const & rhs)
+  template <typename Key_t>
+  bool operator == (Node<Key_t> const & lhs, Node<Key_t> const & rhs)
   {
     return lhs.get_key() == rhs.get_key();
   }
 
-  template <typename Key>
-  bool operator != (Node<Key> const & lhs, Node<Key> const & rhs)
+  template <typename Key_t>
+  bool operator != (Node<Key_t> const & lhs, Node<Key_t> const & rhs)
   {
     return !(lhs == rhs);
   }
 
-  template <typename Key>
-  bool operator < (Node<Key> const & lhs, Node<Key> const & rhs)
+  template <typename Key_t>
+  bool operator < (Node<Key_t> const & lhs, Node<Key_t> const & rhs)
   {
     return lhs.get_key() < rhs.get_key();
   }
