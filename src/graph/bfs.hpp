d3e52a630d08a345d1af90a4028954163cf8dbee
Andrew Robinson <alr104@zips.uakron.edu>
2019-09-11 13:47:07 -0400

    Change graph class and remove old tests that are not applicable anymore. Update Bfs class to accomodate new graph class

--- src/graph/bfs.hpp
+++ src/graph/bfs.hpp
@@ -1,71 +1,71 @@
 #pragma once
 
-#include <list>
-#include <map>
 #include <vector>
+#include <queue>
 
 #include "edge.hpp"
 #include "graph.hpp"
 
 namespace graph {
 
-template <typename Key_t, typename Weight_t>
-class Bfs {
-  struct bfs_node {
-    enum bfs_color { WHITE, GRAY, BLACK };
+  class Bfs {
+  public:
 
-    reset() {
-      bfs_color = bfs_color::WHITE;
-      distance = 0;
-      path.clear();
-      parent = nullptr;
+    struct bfs_node {
+      enum bfs_color { WHITE, GRAY, BLACK };
+
+      bfs_color color;
+      int distance;
+      std::vector<int> path;
+      int parent;
+
+      void reset()
+      {
+        color = bfs_color::WHITE;
+        distance = 0;
+        path.clear();
+        parent = 0;
+      }
+    };
+
+  public:
+    /*
+     Return a vector that has the path from source node to
+     the destination
+     node. This path contains the least
+     ammount of EDGES. Edge weight does not
+     get taken into
+     account in this algorithm.
+    */
+    std::vector<Graph::edge> bfs_shortest_path(Graph G, int source, int sink)
+    {
+      std::vector<Graph::edge> shortest_path;
+
+      std::vector<bfs_node> bfs_nodes;
+      int number_of_nodes = G.get_number_of_nodes();
+
+      for(int i =0; i < number_of_nodes;++i)
+      {
+        bfs_node new_bfs_node;
+        new_bfs_node.reset();
+        bfs_nodes.push_back(new_bfs_node);
+      }
+
+      bfs_nodes[source].color = bfs_node::bfs_color::GRAY;
+      bfs_nodes[source].distance = 0;
+      bfs_nodes[source].parent = -1;
+
+      std::queue<bfs_node*> node_queue;
+      node_queue.push(&bfs_nodes[source]);
+
+
+      return shortest_path;
     }
 
-    bfs_color node_color;
-    int distance;
-    std::vector<int> path;
-    Key_t parent;
+    void reset_bfs_nodes()
+    {
+      
+    }
   };
-
- public:
-  using edge_type = Edge<Key_t, Weight_t>;
-  std::vector<bfs_node> bfs_node_vector;
-
- private:
-  /*
-   Return a vector that has the path from source node to
-   the destination
-   node. This path contains the least 
-   ammount of EDGES. Edge weight does not
-   get taken into
-   account in this algorithm.
-  */
-  std::vector<Key_t> bfs_shortest_path(graph::Graph<Key_t, Weight_t> graph,
-                                       Key_t source, Key_t destination) {
-    std::map<Key_t, bfs_node> nodes;
-    std::map<Key_t, std::list<edge_type>> adjacency_list =
-        graph.get_adjavency_list();
-
-    // iterate through adjacency list and add nodes
-    for (std::map<Key_t, std::list<edge_type>>::iterator iter =
-             adjacency_list.begin();
-         iter != adjacency_list.end(); ++iter) {
-      nodes[iter->first];
-      nodes.at(iter->first).reset();
-    }
-
-    return shortest_path;
-  }
-
-  void reset_bfs_nodes() {
-    for (auto iter = bfs_node_vector.begin(); iter != bfs_node_vector.end();
-         ++iter) {
-      iter->node_color = bfs_node::bfs_color::WHITE;
-      iter->distance = 0;
-      iter->parent = nullptr;
-      iter->path.clear();
-    }
-  }
-};
 
 }  // namespace graph
