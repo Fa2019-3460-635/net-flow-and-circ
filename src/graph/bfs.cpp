ef1a370d144b9321b4069e87737bbd11d297e505
Sean Collins <sgc29@zips.uakron.edu>
2019-09-25 01:20:39 -0400

    Remove ford fulkerson knowledge for BFS

--- src/graph/bfs.cpp
+++ src/graph/bfs.cpp
@@ -7,38 +7,6 @@
 
 namespace graph {
 
-/**
- * A data structure for holding intermediate data for the Breadth-First Search.
- */
-struct bfs_node
-{
-  bfs_node ()
-  : bfs_node(0)
-  {}
-
-  bfs_node (int node_number)
-  : discovered(false),
-    node_number(node_number),
-    minimum_capacity(0)
-  {}
-
-  bool discovered;
-  std::vector<int> path;
-  int node_number;
-  int minimum_capacity;
-
-  /**
-   * @brief Reset all the members of the bfs_node struct
-   *        to defaults
-   */
-  void reset()
-  {
-    discovered = false;
-    node_number = -1;
-    path.clear();
-  }
-};
-
 std::vector<int> Bfs::bfs_shortest_path(Graph & graph, int source, int sink)
 {
   auto adjacency_list = graph.get_adjacency_list();
@@ -81,65 +49,6 @@ std::vector<int> Bfs::bfs_shortest_path(Graph & graph, int source, int sink)
     path.push_back(i);
   std::reverse(std::begin(path), std::end(path));
   return path;
-}
-
-Bfs::bfs_fordfulkerson_data Bfs::bfs_fordfulkerson(FlowNetwork &G, int source, int sink)
-{
-  Bfs::bfs_fordfulkerson_data fordfolkerson_data;
-  int minimum_capacity;
-
-  auto adjacency_list = G.get_adjacency_list();
-
-  // create an adjacency list of "bfs_node" instances
-  std::vector<bfs_node> bfs_nodes;
-  for (int i = 0; i < adjacency_list.size(); ++i)
-    bfs_nodes.emplace_back(i);
-
-  // initialize the "source" node
-  bfs_nodes[source].minimum_capacity = std::numeric_limits<int>::max(); // simulates infinity
-
-  // create a queue of discovered nodes, and initialize with the source node
-  std::queue<bfs_node *> node_queue;
-  node_queue.push(&bfs_nodes[source]);
-
-  while (!node_queue.empty())
-  {
-    bfs_node *current_node = node_queue.front();
-    node_queue.pop();
-
-    auto & edges = adjacency_list[current_node->node_number];
-
-    // for each node adjacent to current node
-    for(int i = 0; i < edges.size(); ++i)
-    { 
-      // if the adjacent node hasn't been visited yet
-      bfs_node & adjacent_node = bfs_nodes[edges[i].node];
-      if(!adjacent_node.discovered)
-      {
-        adjacent_node.discovered = true;
-        adjacent_node.path = current_node->path;
-        adjacent_node.path.push_back(current_node->node_number);
-        adjacent_node.minimum_capacity = current_node->minimum_capacity;
-        int adjacent_capacity = edges[i].weight;
-
-        if(adjacent_capacity < adjacent_node.minimum_capacity) {/* Check if new capacity is less than old capacity*/
-          adjacent_node.minimum_capacity = adjacent_capacity;
-        }
-
-        if(adjacent_node.node_number == sink) {
-          adjacent_node.path.push_back(adjacent_node.node_number);
-          fordfolkerson_data.path = adjacent_node.path;
-          fordfolkerson_data.minimum_capacity = adjacent_node.minimum_capacity;
-          return fordfolkerson_data;
-        }
-        node_queue.push(&adjacent_node);
-      }
-    }
-
-  }
-  std::vector<int> empty_vector;
-  fordfolkerson_data.path = empty_vector;
-  return fordfolkerson_data;
 }
 
 } // namespace graph
--- src/graph/bfs.hpp
+++ src/graph/bfs.hpp
@@ -2,20 +2,12 @@
 
 #include <vector>
 
-#include "graph.hpp"
 #include "flow_network.hpp"
 
 namespace graph {
 
-  class Bfs {
-  public:
-
-    struct bfs_fordfulkerson_data
-    {
-      std::vector<int> path;
-      int minimum_capacity;
-    };
-
+  class Bfs
+  {
   public:
 
     /**
@@ -31,17 +23,6 @@ namespace graph {
      * an empty vector, otherwise.
      */
     static std::vector<int> bfs_shortest_path(graph::Graph &G, int source, int sink);
-
-    /**
-     * @brief give the shortest path and minimum capacity
-     *
-     * @param G: The graph in question
-     * @param source: the start node
-     * @param sink: the destination node
-     * @return A struct with the shortest path from source to sink represented as a std::vector<int>
-     * and the minimum capacity.
-     */
-    static bfs_fordfulkerson_data bfs_fordfulkerson(graph::FlowNetwork &G, int source, int sink);
 
   };
 
