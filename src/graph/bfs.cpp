f1a438ea41af0c9765c7bbeb96fed304970166a0
Sean Collins <sgc29@zips.uakron.edu>
2019-09-24 21:15:10 -0400

    Add some more tests for BFS

--- src/graph/bfs.cpp
+++ src/graph/bfs.cpp
@@ -43,47 +43,42 @@ std::vector<int> Bfs::bfs_shortest_path(Graph & graph, int source, int sink)
 {
   auto adjacency_list = graph.get_adjacency_list();
 
+  // initialize the data structures
   std::vector<bool> discovered (adjacency_list.size(), false);
   std::vector<int> previous_node (adjacency_list.size(), -1);
   std::queue<int> node_queue;
-
   discovered.at(source) = true;
   node_queue.push(source);
+
+  // conduct the breadth-first search
   while (!node_queue.empty())
   {
+    // get next node
     int current = node_queue.front();
     node_queue.pop();
 
-    // detect whether or not we have reached the sink node
+    // stop if sink is found
     if (current == sink)
       break;
 
-    // enqueue any neighbors that have not yet been discovered
+    // process neighbors
     for (auto const & edge : adjacency_list.at(current))
-    {
-      int dest = edge.node;
-      if (discovered.at(dest))
-        continue;
-
-      // cache the previous node
-      previous_node.at(dest) = current;
-
-      // mark next node as discovered
-      discovered.at(dest) = true;
-
-      // add the neighbor to the queue
-      node_queue.push(dest);
-    }
+      if (!discovered.at(edge.node))
+      {
+        discovered.at(edge.node) = true;
+        previous_node.at(edge.node) = current;
+        node_queue.push(edge.node);
+      }
   }
 
-  // return empty path if sink was not reachable
+  // return empty path if sink was unreachable
   if (!discovered.at(sink))
     return {};
 
-  // reconstruct the path
+  // otherwise, reconstruct the path
   std::vector<int> path;
   for (int i = sink; i != -1; i = previous_node.at(i))
-      path.push_back(i);
+    path.push_back(i);
   std::reverse(std::begin(path), std::end(path));
   return path;
 }
